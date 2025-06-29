8d6bdc7fd3938de3d7718789f438dd2f26673a3d
Biswajit-Sao <saobiswajit@gmail.com>
2025-06-29 13:07:16 +0530

    first commit

---
+++ 1_prob.js
@@ -0,0 +1,9 @@
+console.log("Hello Sir")
+
+
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/Biswajit-Sao/JavaScript-Q.git
+// git push -u origin main
