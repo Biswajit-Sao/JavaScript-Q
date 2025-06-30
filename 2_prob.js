11c2d77ca8fbaff1be1c97bcbeb702f53011b64f
Biswajit-Sao <saobiswajit@gmail.com>
2025-06-30 12:38:10 +0530

    first commit

---
+++ 2_prob.js
@@ -0,0 +1,21 @@
+// let const var 
+
+// var a; ✔
+// var a=20; ✔
+
+// let a; ✔
+// let a=10;  ✔
+
+// const a; Error
+// const a=20; ✔
+
+var score=10; //✔
+var score=20; //✔
+
+let age=20; //✔
+age=27; //✔
+// let age=57; Error
+
+const PI=3.14;
+// PI=3.1415; Error
+// const PI=3.1415; Error
